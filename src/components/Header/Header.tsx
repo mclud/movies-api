8e2d40ad257e269c0fbf8e33f01379ffa141171f
mclud <ludovic.maccarthy@abtasty.com>
2022-02-25 11:50:28 +0100

    nav header refact +css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,32 +12,40 @@ function Header() {
   let navCfg = useSelector(selectNavCfg);
 
   return (
-    <Container fluid className="header d-flex flex-column">
-      <nav>
-        <ul className="d-flex justify-space-between align-items-center row">
-          <Col md={3} xs={12}>
-            <li className="brand d-flex align-items-center justify-space-center">
-              <img className="logo" src={logo} alt="logoo" />
-              <div className="logo-title">WATCHOUT</div>
-            </li>
-          </Col>
-          <Col md={7} xs={12} className="categorys">
-            <li>
-              <Link to="/">{{ ...HeaderTrads(navCfg.lang) }.home}</Link>
-            </li>
-            <li>
-              <Link to="/favs">{{ ...HeaderTrads(navCfg.lang) }.favs}</Link>
-            </li>
-            <li>
-              <Link to="/cats">{{ ...HeaderTrads(navCfg.lang) }.cats}</Link>
-            </li>
-            <Searchbar />
-          </Col>
-          <Col md={2} xs={12}>
-            <Flags />
-          </Col>
-        </ul>
-      </nav>
+    <Container fluid className="header d-flex flex-column p-2">
+      <div className="d-flex justify-space-between align-items-center row">
+        <Col md={3} xs={12}>
+          <div className="brand d-flex align-items-center justify-space-center">
+            <img className="logo" src={logo} alt="logoo" />
+            <div className="logo-title">WATCHOUT</div>
+          </div>
+        </Col>
+        <Col md={7} xs={12}>
+          <nav className="row">
+            <Col md={7}>
+              <ul>
+                <li>
+                  <Col>
+                    <Link to="/">{{ ...HeaderTrads(navCfg.lang) }.home}</Link>
+                  </Col>
+                </li>
+                <li>
+                  <Link to="/favs">{{ ...HeaderTrads(navCfg.lang) }.favs}</Link>
+                </li>
+                <li>
+                  <Link to="/cats">{{ ...HeaderTrads(navCfg.lang) }.cats}</Link>
+                </li>
+              </ul>
+            </Col>
+            <Col md={3} xs={12}>
+              <Searchbar />
+            </Col>
+          </nav>
+        </Col>
+        <Col md={2} xs={12}>
+          <Flags />
+        </Col>
+      </div>
     </Container>
   );
 }
